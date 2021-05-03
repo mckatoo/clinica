165d9afbb5a3f804aa5ea047153cd62603d52782
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-03 17:53:31 -0300

    ensure LocalSavePatients does delete old cache on save

--- api/src/domain/data/usecases/local-save-patitents.spec.ts
+++ api/src/domain/data/usecases/local-save-patitents.spec.ts
@@ -1,20 +1,34 @@
 class LocalSavePatients {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
 interface CacheStore {
-
+  delete: () => void
 }
 
 class CacheStore implements CacheStore {
   deleteCallsCount = 0
+
+  delete(): void {
+    this.deleteCallsCount++
+  }
 }
 
 describe('LocalSavePatients', () => {
   test('should not delete cache on init', () => {
     const cacheStore = new CacheStore()
-    
-    const localSavePatients = new LocalSavePatients(cacheStore)
+    new LocalSavePatients(cacheStore)
     expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+
+  test('should delete old cache on save', async () => {
+    const cacheStore = new CacheStore()
+    const localSavePatients = new LocalSavePatients(cacheStore)
+    await localSavePatients.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
   })
 })
