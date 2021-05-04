dfbdfdc22151e0e657d8b4851446ddbbb5921af2
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-04 13:37:52 -0300

    refactor: sut creation to a factory helper method

--- api/src/domain/data/usecases/local-save-patitents.spec.ts
+++ api/src/domain/data/usecases/local-save-patitents.spec.ts
@@ -1,34 +1,46 @@
 class LocalSavePatients {
-  constructor(private readonly cacheStore: CacheStore) {}
+  constructor (private readonly cacheStore: CacheStore) {}
 
-  async save(): Promise<void> {
+  async save (): Promise<void> {
     this.cacheStore.delete()
   }
 }
 
-interface CacheStore {
+export interface CacheStore {
   delete: () => void
 }
 
-class CacheStore implements CacheStore {
+class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
 
-  delete(): void {
+  delete (): void {
     this.deleteCallsCount++
+  }
+}
+
+type SutTypes = {
+  sut: LocalSavePatients
+  cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy()
+  const sut = new LocalSavePatients(cacheStore)
+  return {
+    sut,
+    cacheStore
   }
 }
 
 describe('LocalSavePatients', () => {
   test('should not delete cache on init', () => {
-    const cacheStore = new CacheStore()
-    new LocalSavePatients(cacheStore)
+    const { cacheStore } = makeSut()
     expect(cacheStore.deleteCallsCount).toBe(0)
   })
 
   test('should delete old cache on save', async () => {
-    const cacheStore = new CacheStore()
-    const localSavePatients = new LocalSavePatients(cacheStore)
-    await localSavePatients.save()
+    const { sut, cacheStore } = makeSut()
+    await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
 })
