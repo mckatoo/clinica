acb29e2b6b0f8d4046311f5163c380553f571ec9
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-05 14:17:47 -0300

    change asserts to use array to ensure order and quantity of calls

--- api/src/data/tests/mock-cache.ts
+++ api/src/data/tests/mock-cache.ts
@@ -2,32 +2,40 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SavePatients } from '@/domain/usecases'
 
 export class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0
-  insertCallsCount = 0
+  messages: Array<CacheStoreSpy.Message> = []
   deleteKey: string = ''
   insertKey: string = ''
   insertValues: Array<SavePatients.Params> = []
 
   delete (deleteKey: string): void {
-    this.deleteCallsCount++
+    this.messages.push(CacheStoreSpy.Message.delete)
     this.deleteKey = deleteKey
   }
 
   insert (insertKey: string, value: any): void {
-    this.insertCallsCount++
+    this.messages.push(CacheStoreSpy.Message.insert)
     this.insertKey = insertKey
     this.insertValues = value
   }
 
   simulateDeleteError (): void {
     jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
+      this.messages.push(CacheStoreSpy.Message.delete)
       throw new Error()
     })
   }
 
   simulateInsertError (): void {
     jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+      this.messages.push(CacheStoreSpy.Message.insert)
       throw new Error()
     })
+  }
+}
+
+export namespace CacheStoreSpy {
+  export enum Message {
+    delete,
+    insert
   }
 }
--- api/src/data/usecases/save-patients/local-save-patients.spec.ts
+++ api/src/data/usecases/save-patients/local-save-patients.spec.ts
@@ -25,15 +25,18 @@ describe('LocalSavePatients', () => {
     patients = mockPatient(10)
   })
 
-  test('should not delete cache on init', () => {
+  test('should not or insert delete cache on init', () => {
     const { cacheStore } = makeSut()
-    expect(cacheStore.deleteCallsCount).toBe(0)
+    expect(cacheStore.messages).toEqual([])
   })
 
   test('should delete old cache on save', async () => {
     const { sut, cacheStore } = makeSut()
     await sut.save(patients)
-    expect(cacheStore.deleteCallsCount).toBe(1)
+    expect(cacheStore.messages).toEqual([
+      CacheStoreSpy.Message.delete,
+      CacheStoreSpy.Message.insert
+    ])
     expect(cacheStore.deleteKey).toBe('scheduled')
   })
 
@@ -41,26 +44,29 @@ describe('LocalSavePatients', () => {
     const { sut, cacheStore } = makeSut()
     cacheStore.simulateDeleteError()
     const promise = sut.save(patients)
-    expect(cacheStore.insertCallsCount).toBe(0)
+    expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete])
     expect(promise).rejects.toThrow()
   })
 
   test('should insert new Cache if delete succeeds', async () => {
     const { sut, cacheStore } = makeSut()
     await sut.save(patients)
-    expect(cacheStore.deleteCallsCount).toBe(1)
-    expect(cacheStore.insertCallsCount).toBe(1)
+    expect(cacheStore.messages).toEqual([
+      CacheStoreSpy.Message.delete,
+      CacheStoreSpy.Message.insert
+    ])
     expect(cacheStore.insertKey).toBe('scheduled')
     expect(cacheStore.insertValues).toEqual(patients)
   })
-
+  
   test('should throw if insert throws', async () => {
     const { sut, cacheStore } = makeSut()
     cacheStore.simulateInsertError()
     const promise = sut.save(patients)
+    expect(cacheStore.messages).toEqual([
+      CacheStoreSpy.Message.delete,
+      CacheStoreSpy.Message.insert
+    ])
     expect(promise).rejects.toThrow()
   })
 })
-function patients (patients: any) {
-  throw new Error('Function not implemented.')
-}
