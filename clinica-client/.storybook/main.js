f955469d1d3045a631e17042d82f29faf7b594ed
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-27 16:28:22 -0300

    feat: logo with test and storybook

--- clinica-client/.storybook/main.js
+++ clinica-client/.storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
-  babel: async (options) => ({
-    ...options,
-    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
-  }),
+  addons: ['@storybook/addon-essentials','@storybook/addon-postcss'],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
