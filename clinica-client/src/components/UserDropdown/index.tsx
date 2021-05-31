f02c74c857cb547a1af92cacf09cb997f0d46160
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-31 13:45:12 -0300

    refact(userdropdown): remove wishlist entry

--- clinica-client/src/components/UserDropdown/index.tsx
+++ clinica-client/src/components/UserDropdown/index.tsx
@@ -1,13 +1,11 @@
-import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
-import {
-  AccountCircle,
-  ExitToApp,
-  FavoriteBorder
-} from '@styled-icons/material-outlined'
 import Dropdown from 'components/Dropdown'
 import { signOut } from 'next-auth/client'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+
+import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+import { AccountCircle, ExitToApp } from '@styled-icons/material-outlined'
+
 import * as S from './styles'
 
 export type UserDropdownProps = {
@@ -28,21 +26,15 @@ const UserDropdown = ({ username }: UserDropdownProps) => {
       }
     >
       <S.Nav>
-        <Link href="/profile/me" passHref>
+        <Link href='/profile/me' passHref>
           <S.Link>
             <AccountCircle />
             <span>My profile</span>
           </S.Link>
         </Link>
-        <Link href="/wishlist" passHref>
-          <S.Link title="Wishlist">
-            <FavoriteBorder />
-            <span>Wishlist</span>
-          </S.Link>
-        </Link>
 
         <S.Link
-          role="button"
+          role='button'
           onClick={async () => {
             const data = await signOut({
               redirect: false,
@@ -50,7 +42,7 @@ const UserDropdown = ({ username }: UserDropdownProps) => {
             })
             push(data.url)
           }}
-          title="Sign out"
+          title='Sign out'
         >
           <ExitToApp />
           <span>Sign out</span>
--- clinica-client/src/components/UserDropdown/test.tsx
+++ clinica-client/src/components/UserDropdown/test.tsx
@@ -12,22 +12,21 @@ useRouter.mockImplementation(() => ({
 
 describe('<UserDropdown />', () => {
   it('should render the username', () => {
-    render(<UserDropdown username="Willian" />)
+    render(<UserDropdown username="Milton" />)
 
-    expect(screen.getByText(/willian/i)).toBeInTheDocument()
+    expect(screen.getByText(/milton/i)).toBeInTheDocument()
   })
 
   it('should render the menu', () => {
-    render(<UserDropdown username="Willian" />)
+    render(<UserDropdown username="Milton" />)
 
     // open menu
-    userEvent.click(screen.getByText(/willian/i))
+    userEvent.click(screen.getByText(/milton/i))
 
     expect(
       screen.getByRole('link', { name: /my profile/i })
     ).toBeInTheDocument()
 
-    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /sign out/i })
     ).toBeInTheDocument()
